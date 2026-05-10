5d829b1035237bb28a2624e567e7c487ed008e9b
Gerard Garcia <gerardgarcia1908@gmail.com>
2026-05-10 09:37:26 +0200

    Update version

--- LoPartidet/app.config.js
+++ LoPartidet/app.config.js
@@ -2,7 +2,7 @@ module.exports = {
   expo: {
     name: "LoPartidet",
     slug: "LoPartidet",
-    version: "1.0.0",
+    version: "1.0.1",
     orientation: "portrait",
     icon: "./assets/images/icon.png",
     scheme: "lopartidet",
