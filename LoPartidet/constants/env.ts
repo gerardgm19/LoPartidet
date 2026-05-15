1914c52c2bcd406418549971ffea54f4ad423cda
Gerard Garcia <gerardgarcia1908@gmail.com>
2026-05-15 16:45:55 +0200

    mobile development connection done

--- LoPartidet/constants/env.ts
+++ LoPartidet/constants/env.ts
@@ -8,20 +8,23 @@ function resolveUrl(debug: string, prod: string): string {
   return debug;
 }
 
-const isDebug = false;
+const isDebug = true;
+
 const apiIdentityCloudflareUrl: string = "https://identity.lopartidet.cat";
 const apiLoPartidetCloudflareUrl: string = "https://api.lopartidet.cat";
 const apiIdentityLocalhostUrl: string = "http://localhost:10002";
 const apiLoPartidetLocalhostUrl: string = "http://localhost:10004";
-const apiAndroidIdentityIpUrl: string = "http://127.0.0.1:10001";
-const apiAndroidLoPartidetIprl: string = "http://127.0.0.1:10003";
+const apiAndroidIdentityIpUrl: string = "http://192.168.0.13:10002";
+const apiAndroidLoPartidetIprl: string = "http://192.168.0.13:10004";
 
 export const AUTH_BASE_URL: string = resolveUrl(
-  apiAndroidIdentityIpUrl, //apiIdentityLocalhostUrl,
+  //apiIdentityLocalhostUrl,
+  apiAndroidIdentityIpUrl,
   apiIdentityCloudflareUrl,
 );
 
 export const API_BASE_URL: string = resolveUrl(
-  apiAndroidLoPartidetIprl, // apiLoPartidetLocalhostUrl,
+  //apiLoPartidetLocalhostUrl,
+  apiAndroidLoPartidetIprl,
   apiLoPartidetCloudflareUrl,
 );
