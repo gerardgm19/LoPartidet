e092313d7a40f65d9ea66fc7af874e7aaeefc38c
Gerard Garcia <gerardgarcia1908@gmail.com>
2026-05-29 16:08:56 +0200

    Configure logging

--- LoPartidet.API/LoPartidet.API/Program.cs
+++ LoPartidet.API/LoPartidet.API/Program.cs
@@ -5,52 +5,76 @@ using LoPartidet.API.Services.Interfaces;
 using LoPartidet.API.Services.Validators;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.EntityFrameworkCore;
-var builder = WebApplication.CreateBuilder(args);
+using NLog;
+using NLog.Web;
 
-builder.Services.AddDbContext<LoPartidetContext>(options =>
-    options.UseMySql(
-        builder.Configuration.GetConnectionString("DefaultConnection"),
-        new MySqlServerVersion(new Version(8, 0))));
+var logger = LogManager.Setup()
+    .LoadConfigurationFromFile("nlog.config")
+    .GetCurrentClassLogger();
 
-builder.Services.AddScoped<IMatchesService, MatchesService>();
-builder.Services.AddScoped<IUsersService, UsersService>();
-builder.Services.AddScoped<IPlayerSkillsService, PlayerSkillsService>();
-builder.Services.AddScoped<IMatchValidationService, MatchValidationService>();
-builder.Services.AddScoped<IUserValidationService, UserValidationService>();
-
-builder.Services.AddHttpClient();
-builder.Services.AddHttpClient<IIdentityManagerService, IdentityManagerService>(client =>
+try
 {
-    client.BaseAddress = new Uri(builder.Configuration["IdentityManager:BaseUrl"]!);
-});
+    logger.Info("Starting LoPartidet.API host");
 
-builder.Services.AddAuthentication("RemoteJwt")
-    .AddScheme<AuthenticationSchemeOptions, RemoteJwtAuthHandler>("RemoteJwt", null);
+    var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddCors(options =>
-    options.AddDefaultPolicy(policy =>
-        policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod()));
+    builder.Logging.ClearProviders();
+    builder.Host.UseNLog();
 
-builder.Services.AddControllers();
-builder.Services.AddOpenApi();
+    builder.Services.AddDbContext<LoPartidetContext>(options =>
+        options.UseMySql(
+            builder.Configuration.GetConnectionString("DefaultConnection"),
+            new MySqlServerVersion(new Version(8, 0))));
 
-var app = builder.Build();
+    builder.Services.AddScoped<IMatchesService, MatchesService>();
+    builder.Services.AddScoped<IUsersService, UsersService>();
+    builder.Services.AddScoped<IPlayerSkillsService, PlayerSkillsService>();
+    builder.Services.AddScoped<IMatchValidationService, MatchValidationService>();
+    builder.Services.AddScoped<IUserValidationService, UserValidationService>();
 
-using (var scope = app.Services.CreateScope())
-{
-    scope.ServiceProvider.GetRequiredService<LoPartidetContext>().Database.Migrate();
+    builder.Services.AddHttpClient();
+    builder.Services.AddHttpClient<IIdentityManagerService, IdentityManagerService>(client =>
+    {
+        client.BaseAddress = new Uri(builder.Configuration["IdentityManager:BaseUrl"]!);
+    });
+
+    builder.Services.AddAuthentication("RemoteJwt")
+        .AddScheme<AuthenticationSchemeOptions, RemoteJwtAuthHandler>("RemoteJwt", null);
+
+    builder.Services.AddCors(options =>
+        options.AddDefaultPolicy(policy =>
+            policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod()));
+
+    builder.Services.AddControllers();
+    builder.Services.AddOpenApi();
+
+    var app = builder.Build();
+
+    using (var scope = app.Services.CreateScope())
+    {
+        scope.ServiceProvider.GetRequiredService<LoPartidetContext>().Database.Migrate();
+    }
+
+    if (app.Environment.IsDevelopment())
+    {
+        app.MapOpenApi();
+    }
+
+    app.UseCors();
+
+    app.UseAuthentication();
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
 }
-
-if (app.Environment.IsDevelopment())
+catch (Exception ex)
 {
-    app.MapOpenApi();
+    logger.Error(ex, "LoPartidet.API host terminated unexpectedly");
+    throw;
 }
-
-app.UseCors();
-
-app.UseAuthentication();
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+finally
+{
+    LogManager.Shutdown();
+}
