d2e3381c7db0725d2d295e6648b9a8969d17cb47
Gerard Garcia <gerardgarcia1908@gmail.com>
2026-06-26 23:32:02 +0200

    Add Match time duration in torunament config

---
+++ LoPartidet.API/LoPartidet.API/Migrations/20260626213123_AddTournamentDurationFields.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LoPartidet.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTournamentDurationFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "GapBetweenMatchesMinutes",
+                table: "Tournaments",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "HalfDurationMinutes",
+                table: "Tournaments",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "HalfTimeDurationMinutes",
+                table: "Tournaments",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "GapBetweenMatchesMinutes",
+                table: "Tournaments");
+
+            migrationBuilder.DropColumn(
+                name: "HalfDurationMinutes",
+                table: "Tournaments");
+
+            migrationBuilder.DropColumn(
+                name: "HalfTimeDurationMinutes",
+                table: "Tournaments");
+        }
+    }
+}
